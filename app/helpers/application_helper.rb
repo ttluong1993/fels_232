7aed4629e889e1a7cfc7219931ba6b04d6d2796c
ttuong1993 <ttluong1993@gmail.com>
2017-02-15 11:42:25 +0700

    Finish signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title = ""
-    base_title = t "layouts.title"
+    base_title = t ".basic_title"
     page_title.present? ? "#{page_title} | #{base_title}" : base_title
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "pages#show", page: "home"
   get "/pages/*page", to: "pages#show"
+  get "/signup", to: "users#new"
+  resources :users, only: [:show, :create]
 end

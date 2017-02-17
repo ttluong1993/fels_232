e581ac8cbbfdd98b2617bc410076d1cb6f9c9991
ttuong1993 <ttluong1993@gmail.com>
2017-02-17 09:14:22 +0700

    Implement show, edit, update users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "pages#show", page: "home"
-  get "/pages/*page", to: "pages#show"
+  root "pages#show", static_page: "home"
+  get "/pages/*static_page", to: "pages#show"
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   post "/login", to:"sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users, only: [:show, :create]
+  resources :users, except: [:new, :destroy]
 end

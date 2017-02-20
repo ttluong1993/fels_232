7007f7d6a1ba714957b8fdec0e301acbbdb9f113
ttuong1993 <ttluong1993@gmail.com>
2017-02-20 09:48:00 +0700

    Following users

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,9 @@ class Lesson < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :results, dependent: :destroy
+  
+  scope :updated_at_desc, ->{order updated_at: :desc}
+  scope :following_by, ->user {where "user_id IN (SELECT followed_id
+    FROM relationships  WHERE  follower_id = user_id)
+    OR user_id = user_id", user_id: user.id}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to:"sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users, except: [:new, :destroy]
+  resources :users, except: [:new, :destroy] do
+    resources :relationships, only: [:index, :create, :destroy]
+  end
 end
